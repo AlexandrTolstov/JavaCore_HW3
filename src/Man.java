7dac4122f40149d63ce621aff178d34cadb57522
Alexandr Tolstov <tolstov.alexandr.85@gmail.com>
2024-11-24 12:55:04 +0500

    Home work 4

---
+++ src/Man.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Man {
+    String name;
+    String surname;
+    Date birthday;
+    Gender gender;
+
+    public Man(String name, String surname, Date birthday, Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.gender = gender;
+    }
+}
